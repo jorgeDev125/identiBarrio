4d756a79bbea46805853a9faf388177fe56e21e3
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-06-14 14:33:42 -0500

    fix styles

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function handleSubmit(event) {
+function handleSubmitRegister(event) {
+  event.preventDefault();
+
+  alert("¡Gracias por tu mensaje! Pronto nos pondremos en contacto.");
+
+  event.target.reset(); // Limpia el formulario
+}
+
+function handleSubmitLogin(event) {
   event.preventDefault();
 
   alert("¡Gracias por tu mensaje! Pronto nos pondremos en contacto.");
