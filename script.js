e26db2e8321558f4dac99660265ea1a463038d55
Jorge Andres Vargas <jorgedev125@gmail.com>
2025-06-14 15:55:39 -0500

    fix text

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 function handleSubmitRegister(event) {
   event.preventDefault();
 
-  alert("¡Gracias por tu mensaje! Pronto nos pondremos en contacto.");
+  alert("Usuario registrado correctamente!");
 
   event.target.reset(); // Limpia el formulario
 }
 
-function handleSubmitLogin(event) {
-  event.preventDefault();
-
-  alert("¡Gracias por tu mensaje! Pronto nos pondremos en contacto.");
-
-  event.target.reset(); // Limpia el formulario
-}
